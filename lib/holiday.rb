5de7f0b464055f4b7eeabb00bfb5205cd2092585
Michael Lynch <michael@michaelrlynch.com>
2019-08-24 23:32:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,8 +22,8 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
- holiday_hash[:winter].each do | hashes, supply |
-   hashes << supply
+ holiday_hash[:winter].each do | supplies, supply |
+   supplies << supply
  end
 end
 
