a995c635961170d2872500c92b8451e0ab3ec944
Michael Lynch <michael@michaelrlynch.com>
2019-08-25 00:47:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   return holiday_hash[:winter][:christmas]
-  return holiday_hash[:winter][:new_years]
+  return holiday_hash[:winter][:new_year]
 end
 
 def all_supplies_in_holidays(holiday_hash)
